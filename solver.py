8d573e9526eaba7417fd7590cbe4c8e59fdb2e88
calvlinzx <linzexin1214@gmail.com>
2020-04-21 02:00:06 +1000

    finished solver backend

--- solver.py
+++ solver.py
@@ -9,8 +9,12 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+from typing import List
 
-def visualize(board):
+def visualize(board: List[List[int]]):
+    '''
+    param: board
+    '''
     for i in range(len(board)):
         if i != 0 and i % 3 == 0:
             print('- - - - - - - - - - -')
@@ -22,12 +26,71 @@ def visualize(board):
             else:
                 print(str(board[i][j]) + ' ', end = '')
 
-def findEmpty(board):
+
+def findEmpty(board: List[List[int]]) -> tuple:
+    '''
+    param: board
+    return: tuple of position of empty box
+    '''
     for i in range(len(board)):
         for j in range(len(board[0])):
             if not board[i][j]:
                 return (i,j)
     return None
 
+def valid(board: List[List[int]], num: int, pos: tuple) -> bool:
+    '''
+    param: board
+    param: the number to be filled
+    param: the position of the box to be filled in
+    return: boolean
+    '''
+
+    # Check rows
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num:# and pos[1] != i:
+            return False
+    
+    # Check columns
+    for j in range(len(board)):
+        if board[j][pos[1]] == num:# and pos[0] != j:
+            return False
+    
+    # Check 3*3 box
+    box_y,box_x = pos[0]//3, pos[1]//3
+
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if board[i][j] == num:# and (i,j) != pos:
+                return False
+    return True
+
+def solve(board: List[List[int]]) -> bool:
+    '''
+    param: board
+    return: bool
+    '''
+
+    found = findEmpty(board)
+    if not found:
+        return True
+    row,col = found
+
+    for num in range(1,10):
+        if valid(board, num, (row,col)):
+            board[row][col] = num
+
+            # Solve sub-problem by recursive call
+            if solve(board):
+                return True
+            
+            # Backtrack
+            board[row][col] = 0
+    return False
+
+
+
 visualize(board)
-print(findEmpty(board))
+print("\nSolving Sudoku...\n")
+solve(board)
+visualize(board)
