dd7b3e233088a6ee3c84b8da8fcff89524e56d97
calvlinzx <linzexin1214@gmail.com>
2020-04-22 00:54:36 +1000

    added classes

--- GUI.py
+++ GUI.py
@@ -1,2 +1,51 @@
 import time
 import pygame
+from solver import solve, valid
+
+pygame.font.init()
+
+
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    # Grid constructor
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+
+class Box:
+    rows = 9
+    cols = 9
+
+    # Box constructor
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.tmp = 0
+        self.selected = False
+
+    # Set temporary value
+    def set_tmp(self, val):
+        self.tmp = val
+
+    # Set value
+    def set(self, val):
+        self.value = val
