8e46e1583fc4ac973c170f5538facb372d36a55e
calvlinzx <linzexin1214@gmail.com>
2020-04-22 23:12:14 +1000

    added comments for box

--- GUI.py
+++ GUI.py
@@ -56,21 +56,22 @@ class Box:
         param: window of the application
         """
         font = pygame.font.SysFont('comicsans', 40)
-        gap = self.width / 9
-        x = self.col * gap
-        y = self.row * gap
 
-        # if current box has tmp value but no final value
-        if self.tmp != 0 and self.value == 0:
-            text = font.render(str(self.tmp), True, (128,128,128))
-            win.blit(text, (x+5, y+5))
-        elif self.value != 0:   # final value has been entered
-            text = font.render(str(self.value), True, (0, 0, 0))
-            win.blit(text, (x + gap/2 - text.get_width()/2, y + gap/2 - text.get_height()/2))
+        gap = self.width / 9  # get width/height of each box
+        x = self.col * gap  # get current column
+        y = self.row * gap  # get current row
 
-        # if current box is selected, it has red rectangle surrounded
-        if self.selected:
-            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+        if self.tmp != 0 and self.value == 0:       # if current box has tmp value but no final value
+            text = font.render(str(self.tmp), True, (128, 128, 128))    # render gray text of tmp value
+            win.blit(text, (x + 5, y + 5))      # blit the text on top-left of current box
+        elif self.value != 0:  # final value has been entered
+            text = font.render(str(self.value), True, (0, 0, 0))    # render black text of final value
+            win.blit(text, (x + gap / 2 - text.get_width() / 2, y + gap / 2 - text.get_height() / 2))   # blit on box
+
+        # if current box is selected
+        if self.selected:       # if current box is selected
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)     # surround box with red rectangle of width 3
+
 
 def main():
     window = pygame.display.set_mode((540, 600))
