6087981342226945528770f7d88b4200a1bc95cc
calvlinzx <linzexin1214@gmail.com>
2020-04-24 02:11:16 +1000

    added two methods in GUI

--- GUI.py
+++ GUI.py
@@ -6,14 +6,26 @@ from grid import *
 pygame.font.init()
 
 
-# todo
 def redraw_window(win, grid, timestamp, strikes):
-    pass
+    win.fill((255, 255, 255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0, 0, 0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    grid.draw(win)
 
 
-# todo
-def format_time(sec):
-    pass
+def format_time(secs):
+    sec = secs % 60
+    minute = secs // 60
+    hour = minute // 60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
 
 
 def main():
